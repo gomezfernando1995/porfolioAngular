fca87c1c91ab5dcb66aeb71f2a9aa5985164945e
gomezfernando1995 <gomezfernando1995@gmail.com>
2023-12-15 11:26:51 -0300

    primer commmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//MATERIAL
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    
+    
+
+  ],
+  providers: [NavbarComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
