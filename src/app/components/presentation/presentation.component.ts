b46f32d7e3bd71889c0197f19b020d4cd1d888e9
gomezfernando1995 <gomezfernando1995@gmail.com>
2023-12-29 07:59:19 -0300

    commit desde pc-casa

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   standalone: false,
   // imports: [],
   templateUrl: './presentation.component.html',
-  styleUrl: './presentation.component.scss'
+  styleUrl: './presentation.component.scss',
 })
-export class PresentationComponent {
+export class PresentationComponent  {
 
+  age! : string;
+
+  constructor(){
+    this.myAge();
+  }
+
+  myAge() {
+    let fecha1: Date = new Date(1995, 10, 20); // 1 de enero de 2000
+    let fecha2: Date = new Date(); // Fecha actual
+
+    let diferenciaEnMilisegundos: number = fecha2.getTime() - fecha1.getTime();
+    let diferenciaEnAños: number =  diferenciaEnMilisegundos / (1000 * 60 * 60 * 24 * 365.25);
+     
+    this.age=  (Math.round( diferenciaEnAños)).toString();
+  }
 }
